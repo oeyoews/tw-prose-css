a7406c1cf7afd25a1d39ced0ff50a67dd24b4b63
oeyoews <jyao4783@gmail.com>
2025-05-22 00:00:02 +0800

    fix: add prose-p:mt-0

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-    <div class="antialiased prose prose-indigo prose-gray max-w-none prose-img:my-0 prose-a:no-underline prose-h2:mt-4 prose-blockquote:not-italic dark:prose-invert dark:prose-pre:bg-[#282c34] dark:prose-pre:text-white prose-p:mt-0 prose-ul:my-0 prose-li:my-0">
+    <div class="antialiased prose prose-indigo prose-gray max-w-none prose-img:my-0 prose-a:no-underline prose-h2:mt-4 prose-blockquote:not-italic dark:prose-invert dark:prose-pre:bg-[#282c34] dark:prose-pre:text-white prose-p:my-0 prose-ul:my-0 prose-li:my-0">
       <h1>Hello</h1>
+      <p class="prose-p:mt-0">dmeo</p>
       <h2>dmeo</h2>
       <li>dmeo</li>
       <blockquote>demo</blockquote>

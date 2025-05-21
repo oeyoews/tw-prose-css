2ec17a829f9595620ee2315466fb20027b9edaf4
oeyoews <jyao4783@gmail.com>
2025-05-21 20:26:06 +0800

    feat: update typoe css

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-
-    <div class="antialiased prose prose-indigo prose-gray max-w-none prose-img:my-0 prose-a:no-underline !prose-h2:mt-4 prose-blockquote:not-italic dark:prose-invert dark:prose-pre:bg-[#282c34] dark:prose-pre:text-white prose-p:mt-0 prose-ul:my-0 prose-li:my-0">
+    <div class="antialiased prose prose-indigo prose-gray max-w-none prose-img:my-0 prose-a:no-underline prose-h2:mt-4 prose-blockquote:not-italic dark:prose-invert dark:prose-pre:bg-[#282c34] dark:prose-pre:text-white prose-p:mt-0 prose-ul:my-0 prose-li:my-0">
       <h1>Hello</h1>
       <h2>dmeo</h2>
       <li>dmeo</li>
